4ca65fad526ecd38d5ec84939833472b410d0e83
Pulavamsi2001 <pulavamsi2001@gmail.com>
2024-09-07 13:44:02 +0530

    new commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h1 className="text-4xl font-bold pb-8">About Me</h1>
       <p className="text-lg leading-relaxed max-w-5xl mx-auto">
       I am a recent graduate with a strong foundation in web development and hands-on training from Beaseant Technologies. 
-      I am skilled in building dynamic user interfaces using HTML, CSS, JavaScript, and React JS. On the back-end, 
+      I am skilled in building dynamic user interfaces using HTML, CSS, JavaScript, and ReactJS. On the back-end, 
       I work with Node.js, Express.js, and Socket.io to create robust applications. My database expertise includes 
       PL/SQL, Oracle SQL, PostgreSQL, MySQL, and MongoDB. With experience in development tools like Visual Studio Code 
       and Git, and familiarity with deployment on Vercel and Firebase, I am enthusiastic about applying my skills to 
