95b42ae6975f1366044687aa7d319af63089ef10
Pulavamsi2001 <pulavamsi2001@gmail.com>
2024-09-07 19:30:51 +0530

    changes in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,11 @@ const About = () => {
     <section id="about" className=" text-white text-center py-16 px-8">
       <h1 className="text-4xl font-bold pb-8">Introduction</h1>
       <p className="text-lg font-semibold leading-relaxed max-w-5xl mx-auto">Hi there! I’m Vamsi Krishna Pula, a dedicated Full-Stack Web Developer trained at Besant Technologies.</p>
-      <p className="text-lg leading-relaxed max-w-5xl mx-auto">
+      <p className="text-justify text-lg leading-relaxed max-w-5xl mx-auto">
       I excel in building responsive, user-centric web applications utilizing modern technologies such as React, Node.js, and more. Whether you’re looking to develop a stunning front-end or robust back-end, I’m here to turn your digital ideas into reality. Explore my work and let’s create something amazing together.
  
       </p>
+
 
     </section>
   );
